b76aa0c6c3b8dcfc5c2a5d569ff9aa6d5406c966
Ryan Bigg <git@ryanbigg.com>
2015-10-08 08:48:28 +1100

    A few small pagination fixes

--- web/controllers/person_controller.ex
+++ web/controllers/person_controller.ex
@@ -13,7 +13,7 @@ defmodule Logs.PersonController do
     |> where([m], m.person_id == ^person.id)
     |> where([m], m.hidden == false)
     |> order_by([m], desc: m.created_at)
-    |> Repo.paginate(page: params["page"])
+    |> Repo.paginate(page: params["page"], page_size: 250)
 
     render conn, "show.html",
       person: person, messages: page.entries,

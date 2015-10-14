a6617fb8cdb28bd36b399c6e0218be77fa96cead
Ryan Bigg <git@ryanbigg.com>
2015-10-14 19:24:24 +1100

    Use Ecto.Adapters.SQL.query to make the activity query

--- web/controllers/api/person_controller.ex
+++ web/controllers/api/person_controller.ex
@@ -5,8 +5,7 @@ defmodule Logs.API.PersonController do
   def activity(conn, params) do
     person = Repo.get_by(Person, nick: params["nick"])
     # TODO: is there a way to use the same connection as Ecto does?
-    {:ok, pid} = Postgrex.Connection.start_link(hostname: "localhost", database: "railsbot")
-    result = Postgrex.Connection.query!(pid, "SELECT COUNT(*) as count, created_at::DATE as date FROM messages WHERE person_id = #{person.id} GROUP BY created_at::DATE ORDER BY date DESC", [])
+    { :ok, result } = Ecto.Adapters.SQL.query(Repo, "SELECT COUNT(*) as count, created_at::DATE as date FROM messages WHERE person_id = #{person.id} GROUP BY created_at::DATE ORDER BY date DESC", [])
 
     render conn, activity: result.rows
   end
--- web/views/api/person_view.ex
+++ web/views/api/person_view.ex
@@ -2,8 +2,8 @@ defmodule Logs.API.PersonView do
   use Logs.Web, :view
 
   def render("activity.json", %{activity: activity}) do
-    Enum.map(activity, fn([count, date]) ->
-      date = Timex.Date.from({date.year, date.month, date.day})
+    Enum.map(activity, fn([count, {year, month, day}]) ->
+      date = Timex.Date.from({year, month, day})
       { :ok, date_str } = Timex.DateFormat.format(date, "%Y-%m-%d", :strftime)
       %{count: count, date: date_str }
     end)

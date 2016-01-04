a334a5c77301875c0ca766a8695f9e247aa4410c
Ryan Bigg <git@ryanbigg.com>
2016-01-04 11:37:12 +1100

    yesterday+tomorrow are functions now

--- web/views/channel_view.ex
+++ web/views/channel_view.ex
@@ -1,6 +1,16 @@
 defmodule Logs.ChannelView do
   use Logs.Web, :view
 
+  def yesterday(date) do
+    {:ok, date} = date |> Calendar.Date.advance(-1)
+    date
+  end
+
+  def tomorrow(date) do
+    {:ok, date} = date |> Calendar.Date.advance(1)
+    date
+  end
+
   def pretty_date(date) do
     date |> Calendar.Strftime.strftime!("%Y-%m-%d")
   end

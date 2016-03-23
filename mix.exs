a77fa1c217e2c5febaf2932e453b3e3db5e657fc
Ryan Bigg <git@ryanbigg.com>
2016-03-24 09:00:25 +1100

    Bump dependencies

--- mix.exs
+++ mix.exs
@@ -29,8 +29,8 @@ defmodule Logs.Mixfile do
   #
   # Type `mix help deps` for examples and options
   defp deps do
-    [{:phoenix, "~> 1.0.2"},
-     {:phoenix_ecto, "~> 1.1"},
+    [{:phoenix, "~> 1.1.4"},
+     {:phoenix_ecto, "~> 2.0"},
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.1"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},

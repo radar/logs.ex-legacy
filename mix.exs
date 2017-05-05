bf4a57febb4eb2a54a86fe2763b7bceba7933f6f
Ryan Bigg <git@ryanbigg.com>
2017-05-06 09:52:46 +1000

    Relax Elixir dependency to 1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Logs.Mixfile do
   def project do
     [app: :logs,
      version: "0.0.1",
-     elixir: "~> 1.4",
+     elixir: "~> 1.3",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,

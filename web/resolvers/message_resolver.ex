156b5d096ca7791ce30953484fc7f74c72cc4d55
Ryan Bigg <git@ryanbigg.com>
2017-11-15 13:51:28 +1100

    Remove unused aliases

--- web/resolvers/message_resolver.ex
+++ web/resolvers/message_resolver.ex
@@ -1,5 +1,5 @@
 defmodule Logs.MessageResolver do
-  alias Logs.{Channel, Message, Person, Repo}
+  alias Logs.{Message, Repo}
 
   def by_channel(channel, %{date: date}, _info) do
     {:ok, Message.by_channel_and_date(channel.id, Date.from_iso8601!(date)) |> Repo.all}
